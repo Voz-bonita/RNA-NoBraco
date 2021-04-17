430857a6c39322546631896657eeb7c823d9134d
Jose Vitor <jose.vitor.sem.c@gmail.com>
2021-04-17 17:24:50 -0300

    Various model architectures

--- Keras/Keras_upgrade.py
+++ Keras/Keras_upgrade.py
@@ -1,25 +1,40 @@
 import numpy as np
-from tensorflow.keras.callbacks import TensorBoard
-from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import TensorBoard, EarlyStopping
+from tensorflow.keras.optimizers import Adam, Nadam, Ftrl, SGD
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from Load_Data import X_treino, X_teste, y_treino, y_teste
 
-TenBoard = TensorBoard(log_dir="logs/dense-16x2")
-model = Sequential()
+# Algumas possiveis combinacoes de modelos
+h_layers = range(1, 4)
+neurons = [1, 2, 4, 16, 32, 64]
 
-model.add(Dense(16, activation="tanh", input_shape=X_treino.shape[1:]))
+# Prepara os callbacks de parada
+Estop = EarlyStopping(monitor="loss", patience=8)
 
-model.add(Dense(16, activation="sigmoid"))
 
-model.add(Dense(1, activation="linear"))
+for neuron in neurons:
+    for layers in h_layers:
 
-model.compile(loss="mse",
-              optimizer=Adam(learning_rate=0.1))
+        # Cria-se uma pasta especificando o modelo
+        nome = f"Dense-{layers}-{neuron}"
+        TenBoard = TensorBoard(log_dir=f"logs/{nome}")
 
-model.fit(X_treino, y_treino,
-          batch_size=64,
-          epochs=100,
-          callbacks=[TenBoard],
-          validation_data=(X_teste, y_teste))
+        # Cria-se um novo modelo
+        model = Sequential()
+        model.add(Dense(neuron, activation="sigmoid", input_shape=X_treino.shape[1:]))
 
+        # Adiciona "layer" camadas escondidas
+        for layer in range(layers):
+            model.add(Dense(neuron, activation="relu"))
+
+        model.add(Dense(1, activation="linear"))
+
+        model.compile(loss="mse",
+                      optimizer=SGD(learning_rate=0.003))
+
+        model.fit(X_treino, y_treino,
+                  batch_size=64,
+                  epochs=100,
+                  callbacks=[TenBoard, Estop],
+                  validation_data=(X_teste, y_teste))
