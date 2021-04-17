72470b5036dc98a440877970b77cf552a504718e
Jose Vitor <jose.vitor.sem.c@gmail.com>
2021-04-17 16:44:30 -0300

    Old model adjusted and showed

--- Keras/Keras_old.py
+++ Keras/Keras_old.py
@@ -1,16 +1,16 @@
 import pandas as pd
 import numpy as np
 import pickle
-from tensorflow.keras.callbacks import TensorBoard
+from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint
 from tensorflow.keras.optimizers import SGD
-from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.layers import Dense
 from Load_Data import X_treino, X_teste, y_treino, y_teste
 
-TenBoard = TensorBoard(log_dir="logs/dense-2x1")
+# Callback para salvar apenas o melhor modelo
+MCP_save = ModelCheckpoint('.mdl_wts.hdf5', save_best_only=True, monitor='val_loss', mode='min')
 
-
-# Pesos e vieses fornecidos
+# Pesos e vieses de partida
 W1_Beta1 = [np.empty(shape=(2, 2), dtype=np.float32), np.empty(shape=2, dtype=np.float32)]
 W1_Beta1[0][0] = 0
 W1_Beta1[0][1] = 0
@@ -21,16 +21,15 @@ W2_Beta3[0][0] = 0
 W2_Beta3[1][0] = 0
 
 
+TenBoard = TensorBoard(log_dir=f"logs/Dense-2x1-sigmoid")
 modelo = Sequential()
 
 modelo.add(Dense(2, input_shape=X_treino.shape,
                  activation="sigmoid"))
 modelo.layers[0].set_weights(W1_Beta1)
 
-
 modelo.add(Dense(1, activation="linear"))
 modelo.layers[1].set_weights(W2_Beta3)
-
 
 modelo.compile(loss="mse",
                optimizer=SGD(learning_rate=0.1))
@@ -41,6 +40,9 @@ modelo.fit(X_treino, y_treino,
            batch_size=len(X_treino),
            epochs=100,
            validation_data=(X_teste, y_teste),
-           callbacks=[TenBoard])
+           callbacks=[TenBoard, MCP_save])
 
-modelo.save("2x1-Dense.model")
+# Apenas o melhor modelo e seu vetor de pesos
+model = load_model(".mdl_wts.hdf5")
+print(model.get_weights())
+
