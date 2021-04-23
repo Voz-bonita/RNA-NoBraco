12d25b181be52ca6add0568ec19857eb115081b0
Jose Vitor <jose.vitor.sem.c@gmail.com>
2021-04-23 13:26:53 -0300

    Sumary way

--- Keras/CSVtoGraph.R
+++ Keras/CSVtoGraph.R
@@ -1,48 +1,74 @@
-library(ggplot2)
-library(dplyr)
-library(reshape2)
-library(tibble)
-library(plotly)
-library(htmlwidgets)
-library(purrr)
-library(ggpubr)
+library(pacman)
+pacman::p_load(dplyr, ggplot2, ggpubr, latex2exp, purrr, reshape2, tibble)
+
+path_fix <- function (path) {
+  returnValue(paste0(getwd(), path))
+}
+
+NN_sumary <- function (epochs, layers, neurons, extra = "", endpoint = TRUE) {
+  tab <- tibble(Epochs = seq(1,epochs))
+
+  layers <- as.character(layers)
+  neurons <- as.character(neurons)
+
+  if (extra != "") {
+    extra <- as.character(extra)
+    if (!startsWith(extra,"-")){
+      extra <- paste0("-", extra)
+    }
+  }
+
+  for (layer in layers) {
+    for (neu in neurons) {
+      if (!endpoint & layer == last(layers) & neu == last(neurons)) {
+        break
+      }
+
+      path <- paste0("/Keras/CSV-TensorBoard/run-Dense-",
+                     layer, "-",neu,extra,"_validation-tag-epoch_loss.csv")
+      custo <- tryCatch({read.csv(path_fix(path))$Value},
+               error = function (e) {
+                 message(e)
+                 break
+               })
+      custo
+
+      # Quase nenhum vetor de custos tem 100 observacoes
+      # Causado pelo EarlyStopping
+      n <- length(custo)
+      custo <- c(custo, rep(NA, 100-n))
+
+
+      tab <- add_column(tab, custo, .name_repair = "minimal")
+
+      novo_nome <- paste0("D-",layer,"L-",neu,"N",extra)
+      names(tab)[length(tab)] <- novo_nome
+    }
+  }
+  tab <- melt(tab, id.vars = "Epochs") %>%
+    rename_all(~ c("Epochs", "Arquitetura", "Custo"))
+}
+
 
 # Baixe o csv do TensorBoard e leia-os
-treino <- read.csv(paste0(getwd(), "/Keras/CSV-TensorBoard/run-Old-Dense-2x1_train-tag-epoch_loss.csv")) %>%
+treino <- read.csv(path_fix("/Keras/CSV-TensorBoard/run-Old-Dense-2x1_train-tag-epoch_loss.csv")) %>%
   select(Step, Value)
-teste <- read.csv(paste0(getwd(), "/Keras/CSV-TensorBoard/run-Old-Dense-2x1_validation-tag-epoch_loss.csv")) %>%
+teste <- read.csv(path_fix("/Keras/CSV-TensorBoard/run-Old-Dense-2x1_validation-tag-epoch_loss.csv")) %>%
   select(Step, Value)
 
-EpochsXCusto <- right_join(treino, teste, by = "Step")
-names(EpochsXCusto) <- c("Epoch", "Treino", "Teste")
-EpochsXCusto <- melt(EpochsXCusto, id.vars = "Epoch")
+
+EpochsXCusto <- right_join(treino, teste, by = "Step") %>%
+  rename_all(~ c("Epoch", "Treino", "Teste")) %>%
+  melt(id.vars = "Epoch")
 
 ggplot(data = EpochsXCusto) +
-  geom_line(aes(x = Epoch, y = value, color = variable), size=2) +
+  geom_line(aes(x = Epoch, y = value, color = variable), size=1.3) +
   guides(color = guide_legend(title = "Banco")) +
   ylab("MSE")
 # ggsave(paste0(getwd(),"/Images/Old_SGD.png"))
 
 
-Densemax3x4 <- tibble(Epochs = seq(1,100))
-for (layer in 1:3) {
-  layer <- as.character(layer)
-  for (neurons in c(1,2,4)) {
-    neurons <- as.character(neurons)
-    custo <- read.csv(paste0(getwd(),
-                             "/Keras/CSV-TensorBoard/run-Dense-",
-                             layer, "-",neurons,"_validation-tag-epoch_loss.csv"))$Value
-
-    # Quase nenhum vetor de custos tem 100 observacoes
-    n <- length(custo)
-    custo <- c(custo, rep(NA, 100-n))
-
-    Densemax3x4 <- add_column(Densemax3x4, custo, .name_repair = "minimal")
-  }
-}
-names(Densemax3x4)[2:10] <- c("D-1L-1N", "D-1L-2N", "D-1L-D4N", "D-2L-1N", "D-2L-2N", "D-2L-4N", "D-3L-1N", "D-3L-2N", "D-3L-4N")
-Densemax3x4 <- melt(Densemax3x4, id.vars = "Epochs")
-names(Densemax3x4)[2:3] <- c("Arquitetura", "Custo")
+Densemax3x4 <- NN_sumary(epochs=100, layers=1:3, neurons=c(1,2,4))
 
 ggplot(data = Densemax3x4) +
   geom_line(aes(x = Epochs, y = Custo, color = Arquitetura), size = 1.1) +
@@ -50,24 +76,7 @@ ggplot(data = Densemax3x4) +
 # ggsave(paste0(getwd(),"/Images/Arq-Simples.png"))
 
 
-Densemax2x64 <- tibble(Epochs = seq(1,100))
-for (layer in 1:3) {
-  layer <- as.character(layer)
-  for (neurons in c(16,32,64)) {
-    neurons <- as.character(neurons)
-    custo <- read.csv(paste0(getwd(),
-                             "/Keras/CSV-TensorBoard/run-Dense-",
-                             layer, "-",neurons,"_validation-tag-epoch_loss.csv"))$Value
-
-    n <- length(custo)
-    custo <- c(custo, rep(NA, 100-n))
-
-    Densemax2x64 <- add_column(Densemax2x64, custo, .name_repair = "minimal")
-  }
-}
-names(Densemax2x64)[2:9] <- c("D-1L-16N", "D-1L-32N", "D-1L-D64N", "D-2L-16N", "D-2L-32N", "D-2L-64N", "D-3L-16N", "D-3L-32N")
-Densemax2x64 <- melt(Densemax2x64, id.vars = "Epochs")
-names(Densemax2x64)[2:3] <- c("Arquitetura", "Custo")
+Densemax2x64 <- NN_sumary(epochs = 100, layers=1:3, neurons=c(16,32,64), "", endpoint = F)
 
 comp_plot <- ggplot(data = Densemax2x64) +
   geom_line(aes(x = Epochs, y = Custo, color = Arquitetura), size = 1.1)
@@ -124,18 +133,18 @@ p2 <- pbase + geom_line(data = Lrs[[2]], size = 1.3)
 p3 <- pbase + geom_line(data = Lrs[[3]], size = 1.3)
 p4 <- pbase + geom_line(data = Lrs[[4]], size = 1.3)
 ggarrange(p1,p2,p3,p4)
-# ggsave(paste0(getwd(), "/Images/Multi-lr.png"))
+# ggsave(path_fix("/Images/Multi-lr.png"))
 
 ArcD1 <- LR[c(1,4,7,10,13)] %>%
   melt(id.vars = "Epochs") %>%
   rename("Arquitetura" = "variable", "Custo" = "value")
 pbase + geom_line(data = ArcD1, size = 1.3)
-# ggsave(paste0(getwd(), "/Images/Multi-lr-64N.png"))
+# ggsave(path_fix("/Images/Multi-lr-64N.png"))
 
 
-custo2x32 <- read.csv(paste0(getwd(),
+custo2x32 <- read.csv(path_fix(
                          "/Keras/CSV-TensorBoard/run-Dense-2x32-lr-0.003_validation-tag-epoch_loss.csv"))$Value
-custo2x64 <- read.csv(paste0(getwd(),
+custo2x64 <- read.csv(path_fix(
                          "/Keras/CSV-TensorBoard/run-Dense-2x64-lr-0.003_validation-tag-epoch_loss.csv"))$Value
 min(custo2x32)
 min(custo2x64)
