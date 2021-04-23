55e6d6a25379a5b499918b6aca4e8d1863c2eefd
Jose Vitor <jose.vitor.sem.c@gmail.com>
2021-04-23 12:25:14 -0300

    Using the Neural Network

--- Keras/NN_Predict.py
+++ Keras/NN_Predict.py
@@ -1,10 +1,34 @@
 from tensorflow.keras.models import load_model
-from Load_Data import X_treino, X_teste, y_treino, y_teste
+from Load_Data import X_teste
+from itertools import combinations
 import numpy as np
 
 model = load_model("Dense-2x32-lr-0.003.melhor")
-yhat = model.predict(X_teste)
 
-print(len(yhat))
+# Item c)
+yhat = model.predict(X_teste)
 np.savetxt("optim_predict.csv", yhat, delimiter=",")
 
+
+# Item d)
+x = np.array([np.ones(2)])
+print("d) Previsao:", model.predict(x))
+
+# A rede tem muitos pesos, cuidado com a bagunca
+# print("d) Pesos:", model.get_weights())
+
+
+# Item e)
+
+# Em R, equivale a
+# seq(-3, 3, length.out=n)
+n = 100
+x1 = x2 = np.array([np.linspace(-3, 3, num=n)])
+
+dados_grid = set(combinations(np.append(x1, x2), r=2))
+dados_grid = sorted(dados_grid, key=lambda x: (x[1], x[0]))
+dados_grid_arr = np.array([np.array(comb) for comb in dados_grid])
+
+
+dados_prediction = model.predict(dados_grid_arr)
+np.savetxt("Dados-grid-pred.csv", dados_prediction, delimiter=",")
--- Keras/NN_graphs.R
+++ Keras/NN_graphs.R
@@ -1,5 +1,5 @@
 library(pacman)
-pacman::p_load(dplyr, ggplot2, latex2exp)
+pacman::p_load(dplyr, ggplot2, latex2exp, tibble)
 
 path_fix <- function (path) {
   returnValue(paste0(getwd(), path))
@@ -12,7 +12,8 @@ corte <- 80000
 treinamento <- dados[1:corte,]
 teste <- dados[(corte+1):nrow(dados),]
 
-predito <- as.double(read.csv(path_fix("/Keras/optim_predict.csv")))
+predito <- read.csv(path_fix("/Keras/optim_predict.csv"), header = F) %>%
+  rename("yhat" = "V1")
 
 predictXobs <- tibble(
   yhat = predito$yhat,
@@ -26,6 +27,36 @@ preXobs.plot <- ggplot(predictXobs, aes(x=yhat, y=y)) +
   xlab(TeX("$\\hat{y}$")) + ylab(TeX("y"))
 # preXobs.plot + ggsave(path_fix("/Images/YxYhat_V2.png"))
 
+# Item e)
 
+n <- 100
+x1 <- seq(-3, 3, length.out=n)
+x2 <- seq(-3, 3, length.out=n)
+x.grid <- expand.grid(x1, x2)
+
+# Previsoes
+dados.grid.yhat <- read.csv(path_fix("/Keras/Dados-grid-pred.csv"), header = F) %>%
+  add_column(as_tibble(xgrid), .before = "V1") %>%
+  add_column(rep("NN", nrow(x.grid))) %>%
+  rename_all(~ c("x1", "x2", "mu", "origem"))
+
+
+# Default
+dados.grid <- as_tibble(x.grid) %>%
+  add_column(rep("Default", nrow(x.grid))) %>%
+  rename_all(~ c("x1", "x2", "origem")) %>%
+  mutate(mu=abs(x1^3 - 30*sin(x2) + 10)) %>%
+  add_row(dados.grid.yhat)
+
+
+ggplot(dados.grid, aes(x=x1, y=x2)) +
+  geom_point(aes(colour=mu), size=2, shape=15) +
+  facet_wrap(~ origem) +
+  coord_cartesian(expand=F) +
+  scale_colour_gradient(low="white",
+                        high="black",
+                        name=TeX("E(Y|X_1, X_2)")) +
+  xlab(TeX("X_1")) + ylab(TeX("X_2"))
+# ggsave(path_fix("/Images/Superficie.png"))
 
 
